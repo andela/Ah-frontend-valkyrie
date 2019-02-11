e20d0724ba170437b8485257f1c9cdefefe15c3d
araalifarooq <farooqp35h@gmail.com>
2019-02-11 16:51:50 +0300

    chore(Set up): Set up an empty React project.
    - This commit implements a chore to set up React, Webpack and Babel .
    
    [Delivers #162633451]

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Welcome to Ah-frontend-valkyrie</p>
+      </div>
+    );
+  }
+}
+
+export default App;
