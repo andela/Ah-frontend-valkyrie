b431fdf1fb825bd8c0ae5b266020786b278e0633
Frank Atukunda <fatukunda@gmail.com>
2019-02-12 13:50:10 +0300

    Chore(redux): Set up redux
     - Install redux, react-redux, and redux-thunk.
     - Set up folder structure for redux.
     - Create files for redux store, actions, and a root reducer.
     - Wrap the entire application into the redux Provider component.
    
     [Delivers #162633452]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./App";
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import store from './store/index';
+import App from './App';
 
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
