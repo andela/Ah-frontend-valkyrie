c5a89a19ddad35f6ed2b521f04abb02fbd0f9453
Timothy Mubiru <mubstimor@gmail.com>
2019-02-14 14:52:28 +0300

    "fix(deploy): fix heroku crashes
    - add missing favicon.ico file
    - update package file to remove duplicates

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const path = require( 'path' );
 
 const port = process.env.PORT || 8080;
 const app = express();
-app.use( favicon( path.join( __dirname, '/dist/favicon.ico' ) ) );
-// the __dirname is the current directory from where the script is running
+app.use( favicon( path.join( __dirname, 'dist', 'favicon.ico' ) ) );
+
 app.use( express.static( __dirname ) );
 app.use( express.static( path.join( __dirname, 'dist' ) ) );
 
-app.get( '/ping', function( req, res ){ 
-return res.send( 'pong' );
-} );
-app.get('/*', function (req, res) {
+app.get( '*', ( req, res ) => {
   res.sendFile( path.join( __dirname, 'dist', 'index.html' ) );
-});
+} );
 app.listen( port );
